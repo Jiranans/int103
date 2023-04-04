fcbe82152db7d314bea76a7f563ff5bd046a78d4
Jiranans <97942186+Jiranans@users.noreply.github.com>
2023-04-04 16:40:36 +0700

    Add files via upload

---
+++ mid/int103w05t/src/main/java/human/Gender.java
@@ -0,0 +1,21 @@
+package human;
+
+public enum Gender { MALE, FEMALE, OTHER } // boilerplate code
+
+ /*  public class Gender { 
+    public static Gender MALE = new Gender (0);
+    public static Gender FEMALE = new Gender (1);
+    public static Gender OTHER = new Gender (2);
+    
+    private final int v;
+    public Gender (int v){ this.v = v ;}
+  
+       public String tosString() {
+       return switch(v) {
+          case 0 -> "MALE" ;
+          case 1 -> "FEMALE" ;
+          default -> "OTHER" ;
+       };
+    }
+        
+ } */
